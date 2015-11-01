fc0c84e5833d3ad635c215a1dd2c7c2fbb72c70d
rkoeninger <rkoeninger@att.net>
2015-11-01 13:13:29 -0500

    Added reset button, spaced out buttons, refactored button building code

--- main.lua
+++ main.lua
@@ -1,21 +1,22 @@
 local widget = require("widget")
 
-local p1Life = 20
-local p2Life = 20
+local startingLife = 20
+local p1Life = startingLife
+local p2Life = startingLife
 
 local p1LifeLabel = display.newText(
 	p1Life,
-	display.contentCenterX - 128,
+	display.contentCenterX - 160,
 	display.contentWidth / 4 + 32,
 	native.systemFont,
-	40)
+	64)
 p1LifeLabel:setFillColor(0, 1, 0)
 local p2LifeLabel = display.newText(
 	p2Life,
-	display.contentCenterX + 128,
+	display.contentCenterX + 160,
 	display.contentWidth / 4 + 32,
 	native.systemFont,
-	40)
+	64)
 p2LifeLabel:setFillColor(0, 1, 0)
 
 local p1LifeDown = function (event)
@@ -24,14 +25,14 @@ local p1LifeDown = function (event)
 	end
 	p1Life = p1Life - 1
 	p1LifeLabel.text = p1Life
-	if p1Life < 10 then
+	if p1Life < (startingLife / 2) then
 		p1LifeLabel:setFillColor(1, 0, 0)
 	end
 end
 local p1LifeUp = function (event)
 	p1Life = p1Life + 1
 	p1LifeLabel.text = p1Life
-	if p1Life >= 10 then
+	if p1Life >= (startingLife / 2) then
 		p1LifeLabel:setFillColor(0, 1, 0)
 	end
 end
@@ -41,64 +42,44 @@ local p2LifeDown = function (event)
 	end
 	p2Life = p2Life - 1
 	p2LifeLabel.text = p2Life
-	if p2Life < 10 then
+	if p2Life < (startingLife / 2) then
 		p2LifeLabel:setFillColor(1, 0, 0)
 	end
 end
 local p2LifeUp = function (event)
 	p2Life = p2Life + 1
 	p2LifeLabel.text = p2Life
-	if p2Life >= 10 then
+	if p2Life >= (startingLife / 2) then
 		p2LifeLabel:setFillColor(0, 1, 0)
 	end
 end
 
-local p1LifeDownButton = widget.newButton
-{
-	id = "button1Down",
-	label = "Down",
-	fontSize = 22,
-	font = native.systemFontBold,
-	defaultFile = "images/buttonBlue_100.png",
-	overFile = "images/buttonBlueOver_100.png",
-	onRelease = p1LifeDown
-}
-p1LifeDownButton.x = display.contentCenterX - 128
-p1LifeDownButton.y = display.contentWidth / 4 + 100
-local p1LifeUpButton = widget.newButton
-{
-	id = "button1Up",
-	label = "Up",
-	fontSize = 22,
-	font = native.systemFontBold,
-	defaultFile = "images/buttonBlue_100.png",
-	overFile = "images/buttonBlueOver_100.png",
-	onRelease = p1LifeUp
-}
-p1LifeUpButton.x = display.contentCenterX - 128
-p1LifeUpButton.y = display.contentCenterY - 64
+local reset = function (event)
+	p1Life = startingLife
+	p2Life = startingLife
+	p1LifeLabel.text = p1Life
+	p2LifeLabel.text = p2Life
+	p1LifeLabel:setFillColor(0, 1, 0)
+	p2LifeLabel:setFillColor(0, 1, 0)
+end
 
-local p2LifeDownButton = widget.newButton
-{
-	id = "button2Down",
-	label = "Down",
-	fontSize = 22,
-	font = native.systemFontBold,
-	defaultFile = "images/buttonBlue_100.png",
-	overFile = "images/buttonBlueOver_100.png",
-	onRelease = p2LifeDown
-}
-p2LifeDownButton.x = display.contentCenterX + 128
-p2LifeDownButton.y = display.contentWidth / 4 + 100
-local p2LifeUpButton = widget.newButton
-{
-	id = "button2Up",
-	label = "Up",
-	fontSize = 22,
-	font = native.systemFontBold,
-	defaultFile = "images/buttonBlue_100.png",
-	overFile = "images/buttonBlueOver_100.png",
-	onRelease = p2LifeUp
-}
-p2LifeUpButton.x = display.contentCenterX + 128
-p2LifeUpButton.y = display.contentCenterY - 64
+local lifeButton = function (id, label, onRelease, x, y)
+	local button = widget.newButton
+	{
+		id = id,
+		label = label,
+		fontSize = 22,
+		font = native.systemFontBold,
+		defaultFile = "images/buttonBlue_100.png",
+		overFile = "images/buttonBlueOver_100.png",
+		onRelease = onRelease
+	}
+	button.x = x
+	button.y = y
+end
+
+lifeButton("button1Down", "Down",  p1LifeDown, display.contentCenterX - 160, display.contentWidth / 4 + 128)
+lifeButton("button1Up",   "Up",    p1LifeUp,   display.contentCenterX - 160, display.contentCenterY - 96)
+lifeButton("button2Down", "Down",  p2LifeDown, display.contentCenterX + 160, display.contentWidth / 4 + 128)
+lifeButton("button2Up",   "Up",    p2LifeUp,   display.contentCenterX + 160, display.contentCenterY - 96)
+lifeButton("reset",       "Reset", reset,      display.contentCenterX,       display.contentCenterY)
